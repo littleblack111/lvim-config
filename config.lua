946f086f9f0aa4f3e6c8e6e24c13f0e9f5784c87
littleblack111 <jerrylam110209@gmail.com>
2023-08-13 15:10:30 +0800

    updated

--- config.lua
+++ config.lua
@@ -1,1 +1,24 @@
+-- plugins
+lvim.plugins = {
+    --{ 'xiyaowong/transparent.nvim', config=true },
+    { 'codota/tabnine-nvim' },
+}
+--lvim.builtin.lualine.style = "default"
+lvim.transparent_window = true
 lvim.keys.normal_mode["<leader>,"] = ":lua vim.lsp.buf.code_action()<CR>"
+vim.api.nvim_set_keymap('i', '<C-H>', '<C-w>', { noremap = true })
+--lvim.colorscheme = "tokyonight-storm" -- no transparency so ya
+--vim.cmd("set undofile")
+--vim.api.nvim_command("set runtimepath+=~/.vim_runtime")
+--vim.api.nvim_command("source ~/.vim_runtime/vimrcs/basic.vim")
+--vim.api.nvim_command("source ~/.vim_runtime/vimrcs/filetypes.vim")
+--vim.api.nvim_command("source ~/.vim_runtime/vimrcs/plugins_config.vim")
+vim.api.nvim_command("set runtimepath+=~/.vim_runtime")
+--vim.api.nvim_command("source ~/.vim_runtime/vimrcs/basic.vim")
+vim.api.nvim_command("source ~/.vim_runtime/vimrcs/filetypes.vim")
+vim.api.nvim_command("source ~/.vim_runtime/vimrcs/plugins_config.vim")
+--vim.api.nvim_command("source ~/.vim_runtime/vimrcs/extended.vim")
+vim.api.nvim_command("source ~/.vim_runtime/my_configs.vim")
+--vim.api.nvim_command("source $VIMRUNTIME/syntax/syntax.vim")
+lvim.ignorecase = true
+lvim.swapfile = false
