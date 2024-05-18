f03d4c2f4b00aba3a5013d34604cc7c687c84ded
littleblack111 <littleblack11111@gmail.com>
2024-05-18 19:05:21 +0800

    updates

--- config.lua
+++ config.lua
@@ -1,24 +1,23 @@
--- plugins
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+lvim.colorscheme = "tokyonight-storm"
+
 lvim.plugins = {
-    --{ 'xiyaowong/transparent.nvim', config=true },
-    { 'codota/tabnine-nvim' },
+    { 'github/copilot.vim' }
 }
---lvim.builtin.lualine.style = "default"
+
 lvim.transparent_window = true
+lvim.ignorecase = true
 lvim.keys.normal_mode["<leader>,"] = ":lua vim.lsp.buf.code_action()<CR>"
 vim.api.nvim_set_keymap('i', '<C-H>', '<C-w>', { noremap = true })
---lvim.colorscheme = "tokyonight-storm" -- no transparency so ya
---vim.cmd("set undofile")
---vim.api.nvim_command("set runtimepath+=~/.vim_runtime")
---vim.api.nvim_command("source ~/.vim_runtime/vimrcs/basic.vim")
---vim.api.nvim_command("source ~/.vim_runtime/vimrcs/filetypes.vim")
---vim.api.nvim_command("source ~/.vim_runtime/vimrcs/plugins_config.vim")
-vim.api.nvim_command("set runtimepath+=~/.vim_runtime")
---vim.api.nvim_command("source ~/.vim_runtime/vimrcs/basic.vim")
-vim.api.nvim_command("source ~/.vim_runtime/vimrcs/filetypes.vim")
-vim.api.nvim_command("source ~/.vim_runtime/vimrcs/plugins_config.vim")
---vim.api.nvim_command("source ~/.vim_runtime/vimrcs/extended.vim")
-vim.api.nvim_command("source ~/.vim_runtime/my_configs.vim")
---vim.api.nvim_command("source $VIMRUNTIME/syntax/syntax.vim")
-lvim.ignorecase = true
-lvim.swapfile = false
+
+
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.noexpandtab = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
