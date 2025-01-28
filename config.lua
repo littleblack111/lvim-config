44cb035786defaebd8d710f5b5b5dffc530e2310
littleblack111 <littleblack11111@gmail.com>
2025-01-28 16:19:20 +0800

    fixup! change: copilotchat.nvim -> avante.nvim

--- config.lua
+++ config.lua
@@ -241,63 +241,64 @@ lvim.plugins = {
     --     },
     --     -- See Commands section for default commands if you want to lazy load on them
     -- },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    opts = {
-      provider = "copilot",
-      auto_suggestions_provider = "copilot",
-      copilot = {
-        model = "claude-3.5-sonnet",
+
+    {
+      "yetone/avante.nvim",
+      event = "VeryLazy",
+      lazy = false,
+      version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+      opts = {
+        provider = "copilot",
+        auto_suggestions_provider = "copilot",
+        copilot = {
+          model = "claude-3.5-sonnet",
+        },
+        behaviour = {
+          auto_suggestions = true
+        },
+        hints = { enabled = false },
+        file_selector = {
+          provider = "snacks",
+        }
       },
-      behaviour = {
-        auto_suggestions = true
-      },
-      hints = { enabled = false },
-      file_selector = {
-        provider = "snacks",
-      }
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = true,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
+      -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+      build = "make",
+      dependencies = {
+        "stevearc/dressing.nvim",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        --- The below dependencies are optional,
+        "echasnovski/mini.pick", -- for file_selector provider mini.pick
+        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+        "ibhagwan/fzf-lua", -- for file_selector provider fzf
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        {
+          -- support for image pasting
+          "HakonHarnes/img-clip.nvim",
+          event = "VeryLazy",
+          opts = {
+            -- recommended settings
+            default = {
+              embed_image_as_base64 = true,
+              prompt_for_file_name = false,
+              drag_and_drop = {
+                insert_mode = true,
+              },
             },
           },
         },
-      },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
-      },
-      ft = { "markdown", "Avante" },
+        {
+          -- Make sure to set this up properly if you have lazy=true
+          'MeanderingProgrammer/render-markdown.nvim',
+          opts = {
+            file_types = { "markdown", "Avante" },
+          },
+          ft = { "markdown", "Avante" },
+        }
+      }
     },
-  },
-},
     {
         'MeanderingProgrammer/render-markdown.nvim',
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
